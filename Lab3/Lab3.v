9335d71637f6605a45db4fb547eefd06c640e843
星河 <1821365384@qq.com>
2023-06-11 16:21:07 +0800

    上传实验三。

--- Lab3/Lab3.v
+++ Lab3/Lab3.v
@@ -1,27 +1,29 @@
-Theorem double_denial: forall A, ~~A -> A.
-Proof.
-intros.
-
 Lemma ex1: forall A, ~~~A -> ~A.
 Proof.
-intros.
-destruct H.
-assert(h: A -> ~~A).
-intros.
-
-
-assumption.
-
-
+    unfold not.
+    intros.
+    apply H.
+    intros.
+    apply H1, H0.
 Qed.
 
 Lemma ex2: forall A B, A \/ B -> ~(~A /\ ~B).
 Proof.
-auto.
+    unfold not.
+    intros.
+    destruct H, H0.
+    apply H0.
+    assumption.
+    apply H1.
+    assumption.
 Qed.
 
-Lemma ex3: forall T (P: T -> Prop),
-(~exists x, P x) -> forall x, ~P x.
+Lemma ex3: forall T (P: T -> Prop), 
+    (~exists x, P x) -> forall x, ~P x.
 Proof.
-auto.
+    unfold not.
+    intros.
+    destruct H.
+    exists x.
+    assumption.
 Qed.
